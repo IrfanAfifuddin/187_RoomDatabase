08e6f34066e3e95cf5c7a8a917f610f953629803
IrfanAfifuddin <afifuddinirfanafif@gmail.com>
2025-11-28 11:10:47 +0700

    menambahkan DestinasiHome untuk halaman utama

---
+++ app/src/main/java/com/example/a187_roomdatabase/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.a187_roomdatabase.view.route
+
+import com.example.a187_roomdatabase.R
+
+object DestinasiHome: DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
