10cc3ff5ff2b4fdd76ef53fc7579977b03e57da5
IrfanAfifuddin <afifuddinirfanafif@gmail.com>
2025-11-28 10:05:32 +0700

    membuat fungsi untuk mengambil database siswa

--- app/src/main/java/com/example/a187_roomdatabase/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/a187_roomdatabase/room/DatabaseSiswa.kt
@@ -1,16 +1,26 @@
 package com.example.a187_roomdatabase.room
 
 import android.content.Context
+import androidx.lifecycle.ViewModelProvider.NewInstanceFactory.Companion.instance
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Siswa::class], version = 1, exportSchema = false)
-abstract class DatabaseSiswa : RoomDatabase() {
-    abstract fun siswaDao(): SiswaDao
+@Database(entities = [Siswa::class], version =  1, exportSchema = false)
+abstract class DatabaseSiswa : RoomDatabase(){
+    abstract fun siswaDao() : SiswaDao
 
     companion object {
         @Volatile
-        private var Instance: DatabaseSiswa? = null
+        private var instance: DatabaseSiswa? = null
 
-        fun getDatabase(context: Context): DatabaseSiswa {}
+        fun getDatabase(context : Context): DatabaseSiswa{
+            return (instance?: synchronized(this){
+                Room.databaseBuilder(
+                    context, DatabaseSiswa::class.java,
+                    "siswa_database")
+                    .build().also {instance=it }
+            })
+        }
     }
+}
