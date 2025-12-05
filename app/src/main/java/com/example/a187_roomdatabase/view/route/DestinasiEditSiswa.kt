2c7f8c93dcc57a106c858e7aec5ea081dcd0bf2c
IrfanAfifuddin <afifuddinirfanafif@gmail.com>
2025-12-05 09:58:40 +0700

    menambahkan DestinasiEditSiswa dengan idSiswa

--- app/src/main/java/com/example/a187_roomdatabase/view/route/DestinasiEditSiswa.kt
+++ app/src/main/java/com/example/a187_roomdatabase/view/route/DestinasiEditSiswa.kt
@@ -1,4 +1,12 @@
 package com.example.a187_roomdatabase.view.route
 
-object DestinasiEditSiswa{
+import com.example.a187_roomdatabase.R
+
+object DestinasiEditSiswa : DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+
+    const val itemIdArg = "idSiswa"
+
+    val routeWithArgs = "$route/{$itemIdArg}"
 }
