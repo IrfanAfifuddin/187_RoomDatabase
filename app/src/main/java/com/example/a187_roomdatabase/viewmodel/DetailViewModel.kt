f87f6a9fd82b6595bde56e3ca582db69ec37fa5a
IrfanAfifuddin <afifuddinirfanafif@gmail.com>
2025-12-05 10:03:40 +0700

    mengambil idSiswa dari SavedStateHandle pada ViewModel detail

--- app/src/main/java/com/example/a187_roomdatabase/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/a187_roomdatabase/viewmodel/DetailViewModel.kt
@@ -1,4 +1,22 @@
 package com.example.a187_roomdatabase.viewmodel
 
-class DetailViewMode(
-)
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.a187_roomdatabase.repositori.RepositoriSiswa
+import com.example.a187_roomdatabase.view.route.DestinasiDetailSiswa
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+class DetailViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repositoriSiswa: RepositoriSiswa
+) : ViewModel(){
+
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetailSiswa.itemIdArg])
+
+}
+
